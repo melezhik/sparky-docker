4a7b05fb919f2b25ce142c34119cc93eb024f0e8
Alexey Melezhik <melezhik@gmail.com>
2018-01-12 22:35:52 +0300

    implimenting sparky runtime setup #2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 set -e
+
 cd sparky
 
 if ! test -f /home/sparky/.sparky/projects/db.sqlite3; then
   perl6 db-init.pl6
 fi
 
-BAILADOR=host:0.0.0.0 perl6 bin/sparky-web.pl6 </dev/null &>/dev/null &
+if test -f /home/sparky/.sparky/projects/sparky-setup/sparrowfile; then
+  echo "running sparky runtime setup from /home/sparky/.sparky/projects/sparky-setup/sparrowfile ..."
+  sparrowdo --nosudo --local_mode --sparrowfile /home/sparky/.sparky/projects/sparky-setup/sparrowfile \
+  --format=production
+fi
 
-sparkyd
+#BAILADOR=host:0.0.0.0 perl6 bin/sparky-web.pl6 </dev/null &>/dev/null &
+
+#sparkyd
 
